561852304a6cbe7c58255b58f1a455707e1cf590
Cole Peters <cole@colepeters.com>
2023-04-04 16:10:36 -0500

    Update styles

--- app/pages/_styleguide/cheatsheet.mjs
+++ app/pages/_styleguide/cheatsheet.mjs
@@ -3,40 +3,57 @@ export default function cheatsheet({ html, state }) {
 
   return html`
 <style scope=global>
+body {
+  font-family: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;
+  font-size: 14px;
+}
+
+summary {
+  font-size: 18px;
+}
+
 .my-table {
-  display: table;
-  border-collapse: collapse;
+  display: grid;
   width: 100%;
 }
 
 .my-table li {
-  display: table-row;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 42ch 1fr;
   border-bottom: 1px solid #ccc;
 }
 
-
-.my-table li span {
-  display: table-cell;
-  padding: 5px 10px;
-  text-align: left;
-  vertical-align: middle;
+.my-table li:nth-of-type(odd) {
+  background: hsla(0deg 0% 0% / 4%);
 }
+
 </style>
-<main>
-  <label>Search:<input id=search-box class="border1 border-solid border-current p-2" placeholder="search text"></label>
-  <section id=list>
+<main class='leading3 p0 p5-lg'>
+  <label class='block mb0 mb2-lg'>Search: <input id=search-box class="border1 border-solid border-current p-2" placeholder="search text"></label>
+  <section id="list">
   ${styles.map(section => section.rules.length !== 0 ? `
-    <details open class="m0">
-      <summary>${section.name}</summary>
-      <ul class=my-table>
+    <details open>
+      <summary class="text0 font-bold pt0 pb0">${section.name}</summary>
+      <ul class="my-table">
       ${section.rules.map(rule => `
-        <li class="list-none m-2"><span>${rule.selector}:</span><span> ${rule.rule}</span></li>
+        <li class="list-none pt0 pb0 pl-2 pr-2">
+          <span>${rule.selector.replaceAll(',', ',<br />')}</span>
+          <span>
+            ${rule.rule
+    .replaceAll(',', ', ')
+    .replaceAll(':', ': ')
+    .replaceAll(';', ';<br />')
+    .replaceAll('{', '')
+    .replaceAll('}', '')}
+          </span>
+        </li>
       `).join('')}
       </ul>
       
     </details>
   `: []).join('')
-    }
+}
   </section>
 </main>
 
@@ -70,5 +87,3 @@ document.getElementById('search-box').addEventListener('input', filterList);
 
   `
 }
-
-
