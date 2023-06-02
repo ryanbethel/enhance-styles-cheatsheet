1571fc9a45071d1ed1d01b5b8d3909c703a25c61
kj <kj@begin.com>
2023-06-01 18:19:05 -0700

    Clean up

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const path = require('path')
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const { join } = require('path')
 
 module.exports = {
-
   set: {
     http() {
       return [
         {
           method: 'get',
           path: '/_styleguide/cheatsheet',
-          src: path.join(__dirname, 'src', 'routes'),
+          src: join(__dirname, 'src', 'routes'),
           config: {
-            // shared: false,
             views: true,
           }
         },
       ]
     }
-
   }
 }
-
 
